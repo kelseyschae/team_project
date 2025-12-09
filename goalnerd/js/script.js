b3e0749b404bf300150e91e98a005ce41b2841eb
kelseyschae <kelsschaefermeyer2004@gmail.com>
2025-12-09 10:51:28 -0700

    header fixed

--- goalnerd/js/script.js
+++ goalnerd/js/script.js
@@ -174,8 +174,3 @@ document.addEventListener('DOMContentLoaded', () => {
     initNavigationAnimation();
 });
 
-window.addEventListener("load", () => {
-    const loader = document.getElementById("loader");
-    if (loader) loader.style.display = "none";
-  });
-  
